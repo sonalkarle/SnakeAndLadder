b3adbef92db9ba2e6eab04a97c9ff0c54a4f9fa2
sonal <sonalkarle01@gmail.com>
2021-02-04 23:57:23 +0530

    Repeat till reaches to 100 position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,47 +4,41 @@ namespace SnakeAndLadder
 {
     class Program
     {
-       const int NO_OF_PLAYER = 1;
-       const int POSITION = 0;
-       const int NO_PLAY = 0;
-       const int SNAKE = 1;
-       const int LADDER = 2;
         
         static void Main(string[] args)
         {
-           
             Console.WriteLine("Welcome to snake and ladder program");
-            
-            Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
+            int NO_OF_PLAYER = 1;
+            int POSITION = 0;
+            Console.WriteLine("Number of player: " + NO_OF_PLAYER + " is playing in game at position: " + POSITION);
+
+
             Random random = new Random();
-            int dicenumber = random.Next(0, 7);
-            Console.WriteLine("Roll the dice and get answer " + dicenumber);
-           
-            Random random1 = new Random();
-            int Checkposition = random1.Next(0, 3);
-            switch(Checkposition)
+            while (POSITION != 100)
             {
-                case NO_PLAY:
-                   int newposition = (POSITION);
-                    Console.WriteLine("Position of player not changed " + newposition);
-                    break;
-                case SNAKE:
-                    newposition = (POSITION - dicenumber);
-                    Console.WriteLine("Position of player is decrement and reached to  " + newposition);
-                    break;
-                case LADDER:
-                    newposition = (POSITION + dicenumber);
-                    Console.WriteLine("Position of player is increment and reached to  " + newposition);
-                    break;
-                default:
-                    Console.WriteLine("Invalid action");
-                    break;
-                    
+                int dicenumber = random.Next(0, 7);
+                Console.WriteLine("Roll the dice and get answer " + dicenumber);
+                int Choice = random.Next(0, 3);
+                if (Choice == 0)
+                {
+                    Console.WriteLine("No Play");
+                }
+                else if (Choice == 1)
+                {
+                    POSITION += dicenumber;
+                    Console.WriteLine("Ladder" + POSITION);
 
+                }
+                else
+                {
+                    POSITION -= dicenumber;
+                    Console.WriteLine("Snake" + POSITION);
+                }
 
+                
             }
-            
 
         }
     }
 }
+
