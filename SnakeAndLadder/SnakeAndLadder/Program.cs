4fdf3ee6cdd196dea2a12a5295f5e528c3e39894
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-05 00:55:27 +0530

    Update Program.cs

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,6 +4,7 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        //Constant
        const int NO_OF_PLAYER = 1;
        const int POSITION = 0;
        const int NO_PLAY = 0;
@@ -13,15 +14,16 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
            
-            Console.WriteLine("Welcome to snake and ladder program");
+            Console.WriteLine("Welcome to snake and ladder program");            //welcome message
             
             Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
-            Random random = new Random();
+            Random random = new Random();               //Computatiom
             int dicenumber = random.Next(0, 7);
             Console.WriteLine("Roll the dice and get answer " + dicenumber);
            
             Random random1 = new Random();
             int Checkposition = random1.Next(0, 3);
+            //Option statement
             switch(Checkposition)
             {
                 case NO_PLAY:
