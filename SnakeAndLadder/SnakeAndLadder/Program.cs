b1f453b65a5611e047a92cf990bc492266f6a638
sonal <sonalkarle01@gmail.com>
2021-02-04 22:29:53 +0530

    Player roll the dice

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -10,6 +10,9 @@ namespace SnakeAndLadder
             int NO_OF_PLAYER = 1;
             int POSITION = 0;
             Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
+            Random random = new Random();
+            int dicenumber = random.Next(0, 7);
+            Console.WriteLine("Roll the dice and get answer " + dicenumber);
             
         }
     }
