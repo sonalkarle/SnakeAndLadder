36742cee22c85dde964480667c8b11845ca863e3
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-05 00:54:21 +0530

    Update Program.cs

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -6,11 +6,11 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder program");
-            int NO_OF_PLAYER = 1;
-            int POSITION = 0;
+            Console.WriteLine("Welcome to snake and ladder program");     //welcome message
+            int NO_OF_PLAYER = 1;             //number of player
+            int POSITION = 0;                //Position of player
             Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
-            Random random = new Random();
+            Random random = new Random();               //Computation
             int dicenumber = random.Next(0, 7);
             Console.WriteLine("Roll the dice and get answer " + dicenumber);
             
