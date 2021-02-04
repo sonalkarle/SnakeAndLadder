8020cd40e5cef6da73b6105e7421fb981a716a8f
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-05 00:53:24 +0530

    Update Program.cs

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -6,9 +6,9 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder program");
-            int NO_OF_PLAYER = 1;
-            int POSITION = 0;
+            Console.WriteLine("Welcome to snake and ladder program");  //Welcome message
+            int NO_OF_PLAYER = 1;              //number of player
+            int POSITION = 0;                 //position of player
             Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
             
         }
