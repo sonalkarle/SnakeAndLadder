85004b61a50372acea6fd2c4decedb370e4d23ef
sonal <sonalkarle01@gmail.com>
2021-02-04 22:23:58 +0530

    Game played with single person at zero position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -6,7 +6,11 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello snake and ladder program");
+            Console.WriteLine("Welcome to snake and ladder program");
+            int NO_OF_PLAYER = 1;
+            int POSITION = 0;
+            Console.WriteLine("Number of player: " + NO_OF_PLAYER +  " is playing in game at position: " + POSITION);
+            
         }
     }
 }
